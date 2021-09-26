9389c573286c3eca4a60e22ca908ff4e3cc523ee
Th3Cod3rNow <91435365+Th3Cod3rNow@users.noreply.github.com>
2021-09-27 00:31:41 +0300

    Create HW3.cpp

---
+++ HW3.cpp
@@ -0,0 +1,149 @@
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+void N1()
+{
+    double S, n, p;
+    cout << "Enter S, n, p to find your mounth pay: ";
+    cin >> S >> n >> p;
+
+    (S > 0.) && (n > 0.) && (p > 0.) ? cout << "Your mounth pay: " << (S * (p / 100.) * pow(1. + (p / 100.), n)) / (12. * (pow(1. + (p / 100.), n) - 1.)) << "\n" : cout << "Something was wrong! Please, check your entered values.\n";
+}
+
+void N2()
+{
+    double m, S, n;
+    cout << "Enter m, S, n to find your percent: ";
+    cin >> m >> S >> n;
+
+    if ((S > 0.) && (n > 0.) && (m > 0.))
+    {
+        for (double p = 0.; p > -1.; p += 0.1)
+        {
+            if (m <= (S * (p / 100.) * pow(1. + (p / 100.), n)) / (12. * (pow(1. + (p / 100.), n) - 1.)))
+            {
+                cout << "Your percent is " << p << " !\n";
+                break;
+            }
+        }
+    }
+    else
+    {
+        cout << "Something was wrong! Please, check your entered values.\n";
+    }
+}
+
+void N3()
+{
+    string fileName, fileWay;
+    cout << "Enter name of file you want to create: ";
+    cin >> fileName;
+    cout << "Enter way you want to create your file: ";
+    cin >> fileWay;
+
+    ofstream userF(fileWay + fileName + ".txt");
+
+    if (userF.is_open())
+    {
+        char ans;
+        cout << "File was created!\nDo you want to write smth in your file? (y/n) ";
+        cin >> ans;
+
+        if (ans == 'y')
+        {
+            string userText;
+            cout << "Enter your text here: ";
+            cin >> userText;
+
+            userF << userText;
+        }
+    }
+    else
+    {
+        cout << "Oops... Something was wrong!\n";
+    }
+
+    userF.close();
+}
+
+void N4()
+{
+    string fileName, fileWay, fileText;
+    cout << "Enter name of file you want to read: ";
+    cin >> fileName;
+    cout << "Enter the way to your file: ";
+    cin >> fileWay;
+
+    ifstream userF(fileWay + fileName + ".txt");
+    getline(userF, fileText);
+    for (char letter : fileText)
+    {
+        if (isdigit(letter))
+        {
+            cout << letter << " ";
+        }
+    }
+    cout << "\n";
+    userF.close();
+}
+
+void N5()
+{
+    string userText;
+    cout << "Enter any text here without numbers: ";
+    cin >> userText;
+    sort(userText.begin(), userText.end());
+
+    userText.size() == 30 || isdigit(userText[0]) ? cout << userText << "\n" : cout << "ERROR!\n";
+}
+
+void doAgain();
+
+int main()
+{
+    int NUM;
+    cout << "Enter any number of exercise u want to see: ";
+    cin >> NUM;
+
+    switch (NUM)
+    {
+    case 1:
+        N1();
+        doAgain();
+        break;
+    case 2:
+        N2();
+        doAgain();
+        break;
+    case 3:
+        N3();
+        doAgain();
+        break;
+    case 4:
+        N4();
+        doAgain();
+        break;
+    case 5:
+        N5();
+        doAgain();
+        break;
+    default:
+        cout << "This number is unavailable!";
+        break;
+    }
+
+    return 0;
+}
+
+void doAgain()
+{
+    char ans;
+    cout << "Would you like to continue? (y/n) ";
+    cin >> ans;
+    ans == 'y' ? main() : 0;
+}
