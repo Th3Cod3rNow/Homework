f6c65f27f61a97e466774d19734574d8a4f5f206
Th3Cod3rNow <91435365+Th3Cod3rNow@users.noreply.github.com>
2021-10-13 00:37:49 +0300

    Update HW4.cpp

--- HW4.cpp
+++ HW4.cpp
@@ -26,14 +26,19 @@ void N1()
         bool flag = true;
         cout << "Enter your numbers here: ";
         getline(cin >> ws, userText);
-        
-        for (char c : userText)
+
+        while (userText[0] == ' ')
         {
-            if (c == ' ')
+            userText.erase(userText[0], 1);
+        }
+
+        for (int c = 0; c < userText.size(); c++)
+        {
+            if (userText[c] == ' ')
             {
                 spaces++;
             }
-            if (!isdigit(c) && c != ' ' && c != '-' && c != '.')
+            if (((!isdigit(userText[c])) && (userText[c] != ' ') && (userText[c] != '-') && (userText[c] != '.')) || (userText[c] + userText[c + 1] == ' .') || (userText[c] + userText[c + 1] == '. ') || (userText[c] + userText[c + 1] + userText[c + 2] == ' . ') || (userText[c] + userText[c + 1] + userText[c + 2] == ' - ') || (isdigit(userText[c]) && userText[c + 1] == '-'))
             {
                 flag = false;
                 break;
@@ -42,13 +47,13 @@ void N1()
         if (flag == true)
             userF << userText;
         else
-            cout << "Yoy can use only numbers!\n";
+            cout << "You can use only numbers! For example: 1 -1 1.23 -1.23\n";
     }
     else
         cout << "Oops... Something was wrong!\n";
 
     userF.close();
-
+    
     if (spaces == 9)
     {
         ifstream newUserF(fileWay + fileName + ".txt");
@@ -60,32 +65,32 @@ void N1()
             stringstream x;
             x << s;
             vector<double> array;
-            while (array.size() != 9)
+            while (array.size() != 10)
             {
                 x >> newS;
                 array.push_back(stod(newS));
             }
-            for (int i = 0; i < 9; i++)
+            for (int i = 0; i < 10; i++)
             {
                 sum += array[i];
-            }
-            cout << array[0] << endl;
-            cout << sum << endl;
+            }                                         // c:/users/serpuhov/desktop/
+            cout << setprecision(100) << sum << endl; // 272.4  -323423 34234 -324.34343434343 7 6 5 3 3 10
         }
     }
     else
         cout << "Please, check your numbers. It should be 10!\n";
+    
 }
 
 void N2()
 {
-    
+
 }
 
 
 void N3()
 {
-    
+
 }
 
 void N4()
