4e3d76636d4802502ade87df734791811ebe3341
Th3Cod3rNow <91435365+Th3Cod3rNow@users.noreply.github.com>
2021-12-09 17:58:12 +0300

    Update Sieve.cpp

--- Sieve.cpp
+++ Sieve.cpp
@@ -1,36 +1,42 @@
 #include <iostream>
 #include <cmath>
+#include <string>
 
 using namespace std;
 
 int main()
 {
     int numbers;
-    cout << "Enter last simple digit here : ";
+    cout << "Enter last digit here : ";
     cin >> numbers;
 
-    int* allDigits = new int[numbers - 2];
-    int* simpleDigits = new int[numbers - 2];
-    
-    for (int i = 2; i <= numbers; i++)
+    if (numbers)
     {
-        allDigits[i - 2] = i;
-        simpleDigits[i - 2] = i;
+        int* allDigits = new int[numbers - 2];
+        int* simpleDigits = new int[numbers - 2];
+
+        for (int i = 2; i <= numbers; i++)
+        {
+            allDigits[i - 2] = i;
+            simpleDigits[i - 2] = i;
+        }
+        for (int i = 2; pow(i, 2) < numbers; i++)
+        {
+            if (simpleDigits[i - 2])
+                for (int j = i * 2; j < numbers; j += i)
+                    simpleDigits[j - 2] = 0;
+        }
+        int iterator = 1;
+        for (int k = 0; k <= numbers - 2; k++)
+        {
+            iterator == 1 ? cout << "\t" : cout << "";
+            allDigits[k] == simpleDigits[k] ? cout << "\x1B[92m" << allDigits[k] << "\t\033[0m" : cout << "\x1B[91m" << allDigits[k] << "\t\033[0m";
+            iterator++;
+            iterator % 10 == 0 ? cout << "\n" : cout << "";
+        }
     }
-    for (int i = 2; pow(i, 2) < numbers; i++)
-    {
-        if (simpleDigits[i-2])
-            for (int j = i * 2; j < numbers; j += i)
-                simpleDigits[j-2] = 0;
-    }
-    int iterator = 1;
-    for (int k = 0; k <= numbers - 2; k++)
-    {
-        iterator == 1 ? cout << "\t" : cout << "";
-        allDigits[k] == simpleDigits[k] ? cout << "\x1B[92m" << allDigits[k] << "\t\033[0m" : cout << "\x1B[91m" << allDigits[k] << "\t\033[0m";
-        iterator++;
-        iterator % 10 == 0 ? cout << "\n" : cout << "";
-    }
+    else
+        cout << "Wrong digit!\n";
 
     return 0;
 }
