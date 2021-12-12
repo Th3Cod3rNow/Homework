076bbd4194dc33c03943a72949307663c6ec3d91
Th3Cod3rNow <91435365+Th3Cod3rNow@users.noreply.github.com>
2021-12-12 14:13:23 +0300

    Update rows.cpp

--- rows.cpp
+++ rows.cpp
@@ -2,8 +2,31 @@
 #include <cmath>
 #include <fstream>
 #include <string>
+#include <vector>
 
 using namespace std;
+
+double quantile(vector<double> x, double per) {
+    return x[x.size() * per];
+}
+
+vector <double> sortingBuble(vector<double> x)
+{
+    for (int i = 0; i < x.size() - 1; i++)
+    {
+        for (int j = x.size() - 1; j > i; j--)
+        {
+            if (x[j - 1] > x[j])
+            {
+                double T = x[j - 1];
+                x[j - 1] = x[j];
+                x[j] = T;
+            }
+        }
+    }
+
+    return x;
+}
 
 int main()
 {
@@ -43,74 +66,47 @@ int main()
     {
         cout << "\x1B[93mHEIGHT ANALYSIS\033[0m\n";
         cout << "Enter height (cm) and put on <ENTER>.\nFor ending enter 0 and put on <ENTER>\n";
+
+        double digit;
+        vector<double> heights;
+
+        while (true)
+        {
+            cout << "-> ";
+            cin >> digit;
+            if (digit && digit > 0)
+                heights.push_back(digit);
+            else if (digit == 0)
+                break;
+            else
+                cout << "Your digit less then 0! Try smth different.\n";
+        }
+
+        int quantile75 = quantile(sortingBuble(heights), 0.75);
+        int quantile25 = quantile(sortingBuble(heights), 0.25);
+        int interquantile = quantile75 - quantile25;
+        double summa = 0;
+        int len = 0;
         
-        int j = 0;
-
-        ofstream newF("c:/users/serpuhov/desktop/1.txt");
-        if (newF.is_open())
+        for (auto i : heights)
         {
-            for (int i = 0; ; i++)
+            if (i < quantile75 + 1.5 * interquantile && i > quantile25 - 1.5 * interquantile)
             {
-                double height;
-                cout << "-> ";
-                cin >> height;
-                if (height && height != 0)
-                {
-                    newF << height << " ";
-                }
-                else
-                    break;
+                summa += i;
+                len++;
             }
         }
-        else
-            cout << "Smth was wrong!";
 
-        newF.close();
+        double average = summa / len;
+        int count = 0;
 
-        ifstream f("c:/users/serpuhov/desktop/1.txt");
-        if (f.is_open())
+        for (auto i : heights)
         {
-            string str, word;
-            int k = 0;
-            getline(f, str);
-            for (char c : str)
-            {
-                c == ' ' ? k++ : NULL;
-            }
-            double* numbers = new double[k];
-            int j = 0;
-            for (int c = 0; c < str.size(); c++)
-            {
-                if (str[c] != ' ')
-                {
-                    word += str[c];
-                }
-                else
-                {
-                    numbers[j] = stod(word);
-                    word = "";
-                    j++;
-                }
-            }
-            double summa = 0;
-            int count = 0;
-            int u = 0;
-            for (int c = 0; c < k; c++)
-            {
-                summa += numbers[c];
-                count++;
-            }
-            cout << "The average height: " << summa / count << "\n";
-            for (int c = 0; c < k; c++)
-            {
-                numbers[c] > summa / count ? u++: NULL;
-            }
-            cout << u << " peaople(person) are(is) above average height." << "\n";
+            i > average ? count++ : NULL;
         }
-        else
-            cout << "Smth was wrong!";
 
-        f.close();
+        cout << "The average height: " << average << "cm\n";
+        cout << count << " people(person) are(is) higher then average height.\n";
     }
     else
         cout << "This number unavailable!\n";
