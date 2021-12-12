307347d943168243df176d5c242b8b6aa0b68e0e
Th3Cod3rNow <91435365+Th3Cod3rNow@users.noreply.github.com>
2021-12-12 14:26:11 +0300

    Update txt.cpp

--- txt.cpp
+++ txt.cpp
@@ -1,112 +1,69 @@
 #include <iostream>
 #include <cmath>
 #include <string>
-#include <vector>
+#include <fstream>
 
 using namespace std;
 
-double quantile(vector<double> x, double per) {
-    return x[x.size() * per];
-}
-
-vector <double> sortingBuble(vector<double> x)
-{
-    for (int i = 0; i < x.size() - 1; i++)
-    {
-        for (int j = x.size() - 1; j > i; j--)
-        {
-            if (x[j - 1] > x[j])
-            {
-                double T = x[j - 1];
-                x[j - 1] = x[j];
-                x[j] = T;
-            }
-        }
-    }
-
-    return x;
-}
-
 int main()
 {
-    int ans;
-    cout << "V12(1), V19(2): ";
-    cin >> ans;
+    string alp = "0123456789qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM,.?!\"':;-+=_&%$#@~<>()*/|\\№`[]{}";
+    int* counter = new int[alp.size()];
 
-    if (ans == 1)
+    for (int t = 0; t < alp.size(); t++)
     {
-        int n;
-        cout << "Enter N: ";
-        cin >> n;
-
-        if (n)
-        {
-            double result1 = 0;
-            double result2 = 0;
-
-            for (int k = 1; k <= n; k++)
-            {
-                double summa = 0;
-                for (double l = 1; l <= k; l++)
-                    summa += 1 / l;
-                result1 += pow(summa, k);
-            }
-            for (double k = 1; k <= n; k++)
-            {
-                double summa = 1;
-                for (double l = 1.; l <= k; l++)
-                    summa *= l;
-                result2 += pow(summa, k);
-            }
-            cout << "Result: " << result2 / result1 << endl;
-        }
+        counter[t] = 0;
     }
-    else if (ans == 2)
+
+    ifstream userF("c:/users/serpuhov/desktop/123.txt");
+
+    if (userF.is_open())
     {
-        cout << "\x1B[93mHEIGHT ANALYSIS\033[0m\n";
-        cout << "Enter height (cm) and put on <ENTER>.\nFor ending enter 0 and put on <ENTER>\n";
-
-        double digit;
-        vector<double> heights;
-
-        while (true)
+        string fromFile;
+        while (getline(userF, fromFile))
         {
-            cout << "-> ";
-            cin >> digit;
-            if (digit && digit != 0)
-                heights.push_back(digit);
-            else
-                break;
-        }
-
-        int quantile75 = quantile(sortingBuble(heights), 0.75);
-        int quantile25 = quantile(sortingBuble(heights), 0.25);
-        int interquantile = quantile75 - quantile25;
-        double summa = 0;
-        int len = 0;
-
-        for (auto i : heights)
-        {
-            if (i < quantile75 + 1.5 * interquantile && i > quantile25 - 1.5 * interquantile)
+            for (char i : fromFile)
             {
-                summa += i;
-                len++;
+                counter[alp.find(i)] += 1;
             }
         }
-
-        double average = summa / len;
-        int count = 0;
-
-        for (auto i : heights)
-        {
-            i > average ? count++ : NULL;
-        }
-
-        cout << "The average height: " << average << "cm.\n";
-        cout << count << " people(person) are(is) higher then average height.\n";
     }
     else
-        cout << "This number unavailable!\n";
+        cout << "Smth was wrong!";
+
+    userF.close();
+
+    int maximum = 0;
+    int element;
+    for (int t = 0; t < alp.size(); t++)
+    {
+        if (counter[t] > maximum)
+        {
+            maximum = counter[t];
+            element = t;
+        }
+    }
+
+    ifstream newUserF("c:/users/serpuhov/desktop/123.txt");
+
+    if (newUserF.is_open())
+    {
+        string fromFile;
+        while (getline(newUserF, fromFile))
+        {
+            for (char i : fromFile)
+            {
+                i == alp[element] ? cout << "\x1B[92m" << i << "\033[0m" : cout << i;
+            }
+            cout << "\n";
+        }
+    }
+    else
+        cout << "Smth was wrong!";
+
+    newUserF.close();
+
+    cout << "\n" << alp[element] << " = " << maximum;
 
     return 0;
 }
